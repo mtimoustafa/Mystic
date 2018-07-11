f01087bb882c9aee362de181fe54230a53f04b67
Mohamed Moustafa <mti.moustafa@gmail.com>
2018-07-10 23:33:27 -0400

    Added runes

---
+++ Assets/Scripts/RuneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneController : MonoBehaviour {
+	public enum RuneType { Fire, Water, Earth };
+	public RuneType runeType = RuneType.Fire;
+	public string keyBinding = "a";
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+	  spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(keyBinding))
+		  spriteRenderer.color = ToggleColor(true);
+		else if (Input.GetKeyUp(keyBinding))
+		  spriteRenderer.color = ToggleColor(false);
+	}
+
+	Color ToggleColor(bool on) {
+		if (!on) return Color.white;
+
+    switch (runeType) {
+			case RuneType.Fire:
+			  return Color.red;
+			case RuneType.Water:
+			  return Color.blue;
+			case RuneType.Earth:
+			  return Color.green;
+			default:
+			  return Color.black;
+		}
+	}
+}
