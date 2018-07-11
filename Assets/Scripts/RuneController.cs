6c2be4519f456cbef9e03f6dae8448137cef3442
Mohamed Moustafa <mti.moustafa@gmail.com>
2018-07-10 23:41:54 -0400

    Untabify files (what is this 'tab' heresy)

--- Assets/Scripts/RuneController.cs
+++ Assets/Scripts/RuneController.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RuneController : MonoBehaviour {
-	public enum RuneType { Fire, Water, Earth };
-	public RuneType runeType = RuneType.Fire;
-	public string keyBinding = "a";
+  public enum RuneType { Fire, Water, Earth };
+  public RuneType runeType = RuneType.Fire;
+  public string keyBinding = "a";
 
-	SpriteRenderer spriteRenderer;
+  SpriteRenderer spriteRenderer;
 
-	// Use this for initialization
-	void Start () {
-	  spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+  // Use this for initialization
+  void Start () {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(keyBinding))
-		  spriteRenderer.color = ToggleColor(true);
-		else if (Input.GetKeyUp(keyBinding))
-		  spriteRenderer.color = ToggleColor(false);
-	}
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetKeyDown(keyBinding))
+      spriteRenderer.color = ToggleColor(true);
+    else if (Input.GetKeyUp(keyBinding))
+      spriteRenderer.color = ToggleColor(false);
+  }
 
-	Color ToggleColor(bool on) {
-		if (!on) return Color.white;
+  Color ToggleColor(bool on) {
+    if (!on) return Color.white;
 
     switch (runeType) {
-			case RuneType.Fire:
-			  return Color.red;
-			case RuneType.Water:
-			  return Color.blue;
-			case RuneType.Earth:
-			  return Color.green;
-			default:
-			  return Color.black;
-		}
-	}
+      case RuneType.Fire:
+        return Color.red;
+      case RuneType.Water:
+        return Color.blue;
+      case RuneType.Earth:
+        return Color.green;
+      default:
+        return Color.black;
+    }
+  }
 }
